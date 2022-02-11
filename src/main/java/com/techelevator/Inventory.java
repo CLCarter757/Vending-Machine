81bab6d7d265429922850546b53731b022c9e570
Christopher Carson <christopher.carson14@gmail.com>
2022-02-11 09:38:33 -0500

    Work on ui class

--- src/main/java/com/techelevator/Inventory.java
+++ src/main/java/com/techelevator/Inventory.java
@@ -4,5 +4,50 @@ public class Inventory {
 
     //already grabbed input file
     //use slot identifier and keep as location
-    
+    //get inventory
+    private String name;
+    private double price;
+    private String productType;
+    private double quantity;
+
+
+    public Inventory(String name, double price, String productType, double quantity){
+        this.name = name;
+        this.price = price;
+        this.productType = productType;
+        this.quantity = quantity;
+    }
+//Getters
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public double getQuantity() {
+        return 5;
+    }
+    //Setters
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
 }
--- src/main/java/com/techelevator/UI.java
+++ src/main/java/com/techelevator/UI.java
@@ -1,20 +1,37 @@
 package com.techelevator;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 //Where we print to user and receive input from user
 public class UI {
 
+    Scanner userInput = new Scanner(System.in);
+    private Map<String, Inventory> vendingProduct = new HashMap<>();
+
+    public void makeFIle() {
+
+        System.out.println("What is the file");
+        userInput.useDelimiter(Pattern.compile("|"));
+        String filePath = userInput.nextLine();
+        File userFile = new File(filePath);
+
+
+    }
 
     //main menu
     //(1) Display vending machine items
     //(2) Purchase
     //(3) Exit
 
+
     //(1) Display vending machine items - picked
     // Show list of items in vending machine
-    List<Object> vendingMachineItems = new ArrayList<>();
+    //List<Object> vendingMachineItems = new ArrayList<>();
     //products have slot identifier + purchase price
     String slotIdentifier;
     double purchasePrice;
